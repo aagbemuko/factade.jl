6b36f3c63fe30997ea55b88026c22ddd80ba0a68
Adedotun Agbemuko <adedotun.agbemuko@kuleuven.be>
2020-05-25 13:50:08 +0200

    add the my_f fcn
    my_f is a linear function for trial

--- src/factade.jl
+++ src/factade.jl
@@ -2,4 +2,10 @@ module factade
 
 greet() = print("Hello World!")
 
+include("extra_file.jl")
+
+export my_f
+
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using factade
 using Test
 
+
+
+# 2x+3y
+factade.my_f(2,1)
+
+
+
 @testset "factade.jl" begin
-    # Write your own tests here.
+    @test my_f(2,1)==7
+    @test my_f(2,3)==13
 end
